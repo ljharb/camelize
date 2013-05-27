2322d771557115a95133a5422e07a3f472b65018
Raynos <raynos2@gmail.com>
2013-05-26 22:27:08 -0700

    camelCase the value if it's a string.

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 module.exports = function walk (obj) {
+    if (typeof obj === 'string') return camelCase(key); 
     if (!obj || typeof obj !== 'object') return obj;
     if (Array.isArray(obj)) return obj.map(walk);
     return Object.keys(obj).reduce(function (acc, key) {
-        var camel = key.replace(/[_.-](\w|$)/g, function (_,x) {
-            return x.toUpperCase()
-        });
+        var camel = camelCase(key);
         acc[camel] = walk(obj[key]);
         return acc;
     }, {});
 };
+
+function camelCase(str) {
+    return str.replace(/[_.-](\w|$)/g, function (_,x) {
+        return x.toUpperCase()
+    });
+}
