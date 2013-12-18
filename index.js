e38b6aa03bdd586cd190544515e25d6f9d965a09
James Halliday <mail@substack.net>
2013-12-18 09:57:02 -0800

    shims for ie<9

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ module.exports = function(obj) {
 
 function walk (obj) {
     if (!obj || typeof obj !== 'object') return obj;
-    if (Array.isArray(obj)) return obj.map(walk);
-    return Object.keys(obj).reduce(function (acc, key) {
+    if (isArray(obj)) return map(obj, walk);
+    return reduce(objectKeys(obj), function (acc, key) {
         var camel = camelCase(key);
         acc[camel] = walk(obj[key]);
         return acc;
@@ -17,4 +17,34 @@ function camelCase(str) {
     return str.replace(/[_.-](\w|$)/g, function (_,x) {
         return x.toUpperCase()
     });
+}
+
+var isArray = Array.isArray || function (obj) {
+    return Object.prototype.toString.call(obj) === '[object Array]';
+};
+
+var has = Object.prototype.hasOwnProperty;
+var objectKeys = Object.keys || function (obj) {
+    var keys = [];
+    for (var key in obj) {
+        if (has.call(obj, key)) keys.push(key);
+    }
+    return keys;
+};
+
+function map (xs, f) {
+    if (xs.map) return xs.map(f);
+    var res = [];
+    for (var i = 0; i < xs.length; i++) {
+        res.push(f(xs[i], i));
+    }
+    return res;
+}
+
+function reduce (xs, f, acc) {
+    if (xs.reduce) return xs.reduce(f, acc);
+    for (var i = 0; i < xs.length; i++) {
+        acc = f(acc, xs[i], i);
+    }
+    return acc;
 }
